6ff4ea7a7e60ce918219bdd28bedd6691434f8a5
frdrckaman <frdrckaman@gmail.com>
2023-04-13 23:55:28 +0300

    remove unused codes

--- reports/cust_acc_list.py
+++ reports/cust_acc_list.py
@@ -2,15 +2,6 @@ import sqlalchemy as db
 import pandas as pd
 from datetime import datetime
 
-
-username = ''
-password = ''
-database = ''
-port = '1433'
-TDS_Version = '8.0'
-server = ''
-driver = 'FreeTDS'
-
 engine = db.create_engine("mysql+mysqlconnector://rck:frdrck1@localhost:3306/ram")
 
 myQuery = "SELECT * FROM frd"
