12d968df782d804f59430ef32c90fc529ee9f9a6
frdrckaman <frdrckaman@gmail.com>
2023-04-17 23:43:34 +0300

    updates

--- reports/test_report.py
+++ reports/test_report.py
@@ -1,10 +1,7 @@
-import sqlalchemy as db
 import pandas as pd
 from datetime import datetime
 from schedules import settings
 from stg import test_report_stg
-
-# engine = db.create_engine("mysql+mysqlconnector://rck:frdrck1@localhost:3306/ram")
 
 # myQuery = "SELECT * FROM frd"
 df = pd.read_sql_query(test_report_stg.myQuery, settings.engine)
