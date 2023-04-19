ecda3fb7b499f1e4e1f45bd773c86fe02e97a72f
frdrckaman <frdrckaman@gmail.com>
2023-04-19 22:27:30 +0300

    updates

--- reports/cust_acc_list.py
+++ reports/cust_acc_list.py
@@ -1,8 +1,9 @@
 import sqlalchemy as db
 import pandas as pd
 from datetime import datetime
+from schedules import settings
 
-engine = db.create_engine("mysql+mysqlconnector://rck:frdrck1@localhost:3306/ram")
+engine = db.create_engine(settings.MYSQL_CONN)
 
 myQuery = "SELECT * FROM frd"
 df = pd.read_sql_query(myQuery, engine)
